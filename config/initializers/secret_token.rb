f51a5d089e7d5d25f4b87ccb65d9b3063ed43583
bustta <bustta80980@gmail.com>
2014-04-15 22:37:07 +0800

    first commit and add devise authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlapApp::Application.config.secret_key_base = '997e4babae2b4403397308d09a8d87fd156d75f97e83169ee15dba8c0280b4037113f8c9174aa6cbcc15c20c2e3852ff948ece9bca6a730a7bb2568454ebb0e4'
