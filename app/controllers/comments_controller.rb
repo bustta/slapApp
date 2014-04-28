a1f9817a942986ce7afe10e7fab82404bd2ef095
bustta <bustta80980@gmail.com>
2014-04-29 00:49:02 +0800

    add comment UD and view's scss

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,36 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @comment = @topic.comments.find(params[:id])
+
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @comment = @topic.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to topic_path(@topic)
+    else
+      render :edit
+
+    end
+
+
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @comment = @topic.comments.find(params[:id])
+
+    @comment.destroy
+
+    redirect_to topic_path(@topic)
+
+  end
+
 
 
   private
